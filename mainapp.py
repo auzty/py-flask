f27109de3f6a988bd96354e5c48fa16669f20e60
yusuf <thea.uzty@gmail.com>
2022-10-13 16:28:45 +0700

    [DONE] Uploading csv and get avg

--- mainapp.py
+++ mainapp.py
@@ -1,8 +1,11 @@
-import os
+import os,secrets
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, reqparse
-import pandas as pd
 from werkzeug.utils import secure_filename
+from os.path import join, dirname, realpath
+import pandas as pd
+import numpy as np
+
 
 UPLOAD_FOLDER = './tmp'
 ALLOWED_EXTENSIONS = {'csv'}
@@ -15,28 +18,44 @@ api = Api(app)
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+@app.route('/')
+def index():
+    return {'msg':"helllo world, you're accessing /"},200
 
-class Index(Resource):
-    def get(self):
-        data = pd.read_csv('users.csv')  # read local CSV
-        data = data.to_dict()  # convert dataframe to dict
-        return {'data': data}, 200  # return data and 200 OK
+@app.route("/compute", methods=['POST'])
+def uploadFiles():
 
-    def post(self):
-        data = request.json
-        print(request.json)
-        if 'file' not in request.files:
-            return {'msg': 'No files found'}, 400
         
-        file = request.files['file']
+    if 'file' not in request.files:
+        return {'msg': 'File Not Found'},400
 
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-            # return jsonify(data)
-            return {'msg':'file uploaded'}, 200
+    file = request.files['file']
 
-api.add_resource(Index, '/compute')  # add endpoints
+    print(request.files['file'])
+
+    if file and allowed_file(file.filename):
+        #filename = secure_filename(file.filename)
+
+        #generating random filename.csv
+        filename = secrets.token_hex(15)+".csv"
+
+        # init the fullpath variable
+        fullpath = os.path.join(app.config['UPLOAD_FOLDER'],filename)
+
+        # save file to path
+        file.save(fullpath)
+
+        # Processing the CSV
+        result = processCSV(fullpath)
+        return {'status': "ok","average": result}, 200
+    return {'status': "error","msg":'Format Not Supported'}, 400
+
+def processCSV(path):
+    #csv = np.genfromtxt(path, delimiter=',' , names=True)
+    df = pd.read_csv(path,sep=',')
+    print(df)
+    numpyresult = df.to_numpy()
+    return np.average(numpyresult[:,1])
 
 if __name__ == '__main__':
     app.run()  # run our Flask app
